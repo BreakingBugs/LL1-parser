6e1e7cd0c0bdcddbb5ae007c7d4a211ee5f4a235
jmayalag <jayala1993@outlook.com>
2016-11-15 16:17:08 -0300

    Added indirect recursion example

--- sample.py
+++ sample.py
@@ -8,6 +8,9 @@ bnf_text = "E -> T E'\n" \
            "T' -> * F T' | ε\n" \
            "F -> ( E ) | id"
 
+indirect_recursive = "S -> A a | b\n" \
+                     "A -> A c | S d | ε"
+
 bnf_recursive = "E -> E + T | T\n" \
                 "T -> T * F | F\n" \
                 "F -> ( E ) | id"
@@ -33,15 +36,22 @@ print("Parsing Table: ")
 for k, v in table.items():
     print("{}: {}".format(k, v))
 
-
 second_text = "E -> pa Q R | pa Q S | pa T\n" \
-             "U -> e"
+              "U -> e"
 
 third_text = "S -> i E t S | i E t S e S | a\n" \
-               "E -> b"
+             "E -> b"
 print("\n\nBefore left factoring: \n\n")
 print(second_text)
 print("\n\nLeft factoring: \n\n")
 g2 = parse_bnf(second_text)
 g2 = remove_left_factoring(g2)
 g2.print_join_productions()
+
+g3 = parse_bnf(indirect_recursive)
+print("\nBefore removing left recursion")
+g3.print_join_productions()
+
+g3 = remove_left_recursion(g3)
+print("\nAfter removing left recursion")
+g3.print_join_productions()
