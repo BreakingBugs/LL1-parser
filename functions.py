46d0cda1275bf65eb6aed4723c030e56ac34e10f
jmayalag <jayala1993@outlook.com>
2016-11-15 16:08:03 -0300

    Added more conditions for indirect left-recursion

--- functions.py
+++ functions.py
@@ -1,154 +1,6 @@
 # -*- coding: utf-8 -*-
-
-class Rule:
-    def __init__(self, head, body):
-        self.head = head
-        self.body = body
-
-    def __str__(self):
-        return "{} -> {}".format(self.head, ' '.join(self.body))
-
-    def __repr__(self):
-        return "{} -> {}".format(self.head, self.body)
-
-
-class Grammar:
-    def __init__(self, productions=None, start=None, epsilon='ε', eof='$'):
-        self.productions = productions if productions else []
-        self.start = start
-        self.nonterminals = {p.head for p in self.productions}
-        self.epsilon = epsilon
-        self.eof = eof
-
-    def add_rule(self, rule):
-        for i, p in enumerate(self.productions):
-            if p.head == rule.head and p.body == rule.body:
-                return
-        self.productions.append(rule)
-        self.nonterminals = self.nonterminals.union({rule.head})
-
-    def remove_rule(self, rule):
-        for i, p in enumerate(self.productions):
-            if p.head == rule.head and p.body == rule.body:
-                self.productions.pop(i)
-
-    def is_terminal(self, s):
-        return s not in self.nonterminals
-
-    def is_start_symbol(self, symbol):
-        return self.start == symbol
-
-    def productions_for(self, a):
-        """
-        Get productions associated to a nonterminal
-        :param a: the nonterminal
-        :return: list of a-productions
-        """
-        a_productions = [r.body for r in self.productions if r.head == a]
-        return a_productions
-
-    def first(self, x):
-        """
-        Compute FIRST(X)
-        1- If X is a terminal, FIRST(X) = {X}
-        2- If there exists a production X -> ε, FIRST(X) = {ε}
-        3- If there exists a production X -> Y1Y2...Yk, FIRST(X) = {Y1Y2...Yk}
-        :param x:
-        :return: FIRST set
-        """
-        f = set()
-        if isinstance(x, list):
-            f = self.first_multiple(x)
-        elif self.is_terminal(x):
-            f = {x}  # Rule 1 and 2
-        else:
-            for p in self.productions_for(x):
-                f = f.union(self.first(p))
-
-        return f
-
-    def first_multiple(self, tokens):
-        """
-        Compute FIRST(Y1Y2...Yk)
-        :param tokens: list of symbols
-        :return: FIRST set
-        """
-        f = set()
-
-        for t in tokens:
-            ft = self.first(t)
-            f = f.union(ft)
-            if self.epsilon not in ft:
-                break
-
-        return f
-
-    # TODO: test edge cases
-    def follow(self, nonterminal):
-        """
-        Compute FOLLOW(A)
-        :param nonterminal: the nonterminal A
-        :return: set of terminals that can appear immediately to the right of A in some partial derivation
-
-        1.  For each production X -> aAb, put FIRST(b) − {ε} in FOLLOW(A)
-        2.a For each production X -> aAb, if ε is in FIRST(b) then put FOLLOW(X) into FOLLOW(A)
-        2.b For each production X -> aA, put FOLLOW(X) into FOLLOW(A)
-        """
-        f = set()
-        if self.is_start_symbol(nonterminal):
-            f.add(self.eof)
-
-        for p in self.productions:
-            if nonterminal in p.body:
-                position = p.body.index(nonterminal)
-                a = p.body[0:position]
-                b = p.body[position + 1:]
-
-                # Case 1
-                if a and b:
-                    f = f.union(self.first(b) - {self.epsilon})
-                # Case 2.a
-                if a and not b:
-                    f = f.union(self.follow(p.head))
-                    break
-                # Case 2.b
-                if a and b and self.epsilon in self.first(b):
-                    f = f.union(self.follow(p.head))
-                    break
-
-        return f
-
-    def parsing_table(self):
-        table = {}
-        for r in self.productions:
-            terminals = self.first(r.body)
-            for t in terminals:
-                if not self.is_terminal(t):
-                    continue
-                if t == self.epsilon:
-                    f = self.follow(r.head)
-                    for ef in f:
-                        if(table.get((r.head, ef))):
-                            pass #TODO Ambiguity found
-                        else:
-                            table[(r.head, ef)] = r
-                else:
-                    if (table.get((r.head, t))):
-                        pass  # TODO Ambiguity found
-                    else:
-                        table[(r.head, t)] = r
-        return table
-
-    def print_join_productions(self):
-        for x in self.nonterminals:
-            bodies = [' '.join(p) for p in self.productions_for(x)]
-            print("{} -> {}".format(x, ' | '.join(bodies)))
-
-    def __str__(self):
-        return '\n'.join([str(p) for p in self.productions])
-
-    def __repr__(self):
-        return '\n'.join([repr(p) for p in self.productions])
+from grammar import Grammar
+from rule import Rule
 
 
 def parse_bnf(text):
@@ -183,17 +35,43 @@ def parse_bnf(text):
     return g
 
 
+def __normalize_productions(grammar):
+    """
+    Remove empty symbols from productions
+    :param grammar: input grammar
+    :return: normalized grammar
+    """
+    normalized_grammar = Grammar(productions=grammar.productions[::], start=grammar.start,
+                                 epsilon=grammar.epsilon, eof=grammar.eof)
+
+    for p in grammar.productions:
+        if len(p.body) > 1:  # exclude productions of the form X -> ε
+            p.body = [x for x in p.body if x != grammar.epsilon]
+
+    return normalized_grammar
+
+
 # TODO: Definir un ordenamiento
 def nonterminal_ordering(grammar):
     return [grammar.start] + list(grammar.nonterminals - {grammar.start})
 
 
-def generate_key(grammar, x):
-    new_x = '{}'.format(x)
+def __generate_key(grammar, x):
+    new_x = x
     while new_x in grammar.nonterminals:
         new_x += '\''
 
     return new_x
+
+
+def is_left_recursive(head, body):
+    """
+    Check if production is left-recursive (immediate)
+    :param head: right side of production
+    :param body: left side of production
+    :return: True if production is left-recursive, False otherwise
+    """
+    return body and head == body[0]
 
 
 def remove_immediate_left_recursion(grammar, A):
@@ -216,7 +94,7 @@ def remove_immediate_left_recursion(grammar, A):
     new_productions = []
 
     for p in productions:
-        if p and A == p[0]:
+        if is_left_recursive(A, p):
             recursive.append(p)
         else:
             nonrecursive.append(p)
@@ -224,7 +102,7 @@ def remove_immediate_left_recursion(grammar, A):
     if not recursive:
         return [Rule(A, p) for p in productions]  # Return same productions
 
-    new_A = generate_key(grammar, A)
+    new_A = __generate_key(grammar, A)
     for b in nonrecursive:
         # A -> b1 A' | ... | bn A'
         new_productions.append(Rule(A, b + [new_A]))
@@ -243,8 +121,10 @@ def remove_left_recursion(grammar):
     """
     Remove all left recursions from grammar
     :param grammar: input grammar
-    :return: equivalent grammar with no left-recursion
+    :return: equivalent grammar with no left-recursions
     """
+    temp_grammar = Grammar(productions=grammar.productions[::], start=grammar.start,
+                           epsilon=grammar.epsilon, eof=grammar.eof)
     new_grammar = Grammar(start=grammar.start, epsilon=grammar.epsilon, eof=grammar.eof)
     nonterminals = nonterminal_ordering(grammar)
 
@@ -252,40 +132,31 @@ def remove_left_recursion(grammar):
         ai = nonterminals[i]
         for j in range(0, i):
             aj = nonterminals[j]
-            for p_ai in grammar.productions_for(ai):
+            for p_ai in temp_grammar.productions_for(ai):
                 # For each production of the form Ai -> Aj y
                 if p_ai and aj == p_ai[0]:
-                    for p_aj in grammar.productions_for(aj):
-                        grammar.remove_rule(Rule(ai, p_ai))
-                        grammar.add_rule(Rule(ai, p_aj + p_ai[1:]))
+                    replaced_productions = [Rule(ai, p_aj + p_ai[1:]) for p_aj in temp_grammar.productions_for(aj)]
+                    can_remove_productions = any(map(lambda x: is_left_recursive(x.head, x.body), replaced_productions))
+                    # Replace productions only if there were left-recursive ones
+                    if can_remove_productions:
+                        temp_grammar.remove_rule(Rule(ai, p_ai))
+                        for p in replaced_productions:
+                            temp_grammar.add_rule(p)
 
-        new_productions = remove_immediate_left_recursion(grammar, ai)
+        new_productions = remove_immediate_left_recursion(temp_grammar, ai)
         for p in new_productions:
             new_grammar.add_rule(p)
 
-    return new_grammar
+    return __normalize_productions(new_grammar)
 
-
-def normalize_productions(grammar):
-    """
-    Remove empty symbols from productions
-    :param grammar: input grammar
-    :return: normalized grammar
-    """
-    normalized_grammar = Grammar(productions=grammar.productions, start=grammar.start,
-                                 epsilon=grammar.epsilon, eof=grammar.eof)
-
-    for p in grammar.productions:
-        if len(p.body) > 1:  # exclude productions of the form X -> ε
-            p.body = [x for x in p.body if x != grammar.epsilon]
-
-    return normalized_grammar
 
 def check_items_equal(l):
     return l[1:] == l[:-1]
 
+
 def get_max_length(lst):
     return max(map(lambda l: len(l), lst))
+
 
 def get_prefixes(grammar, productions):
     common = {}
@@ -295,24 +166,26 @@ def get_prefixes(grammar, productions):
             common.setdefault(x[0], []).append(x)
     for k, v in common.items():
         common_index = 1
-        sublist = map(lambda l: l[0:common_index+1], v)
+        sublist = map(lambda l: l[0:common_index + 1], v)
         while check_items_equal(sublist) and common_index < get_max_length(v):
             common_index += 1
-            sublist = map(lambda l: l[0:common_index+1], v)
+            sublist = map(lambda l: l[0:common_index + 1], v)
         common_index = common_index - 1
-        if(len(v)>1):
-            common[k] = map(lambda l: l[common_index+1:], v)
+        if (len(v) > 1):
+            common[k] = map(lambda l: l[common_index + 1:], v)
         if common_index > 0:
-            common[k] = map(lambda l: l[common_index+1:], v)
-            final_key = ' '.join(v[0][0:common_index+1])
+            common[k] = map(lambda l: l[common_index + 1:], v)
+            final_key = ' '.join(v[0][0:common_index + 1])
             common[final_key] = common[k]
             del common[k]
 
     return common
 
+
 def are_there_factors(lst):
     first_elements = map(lambda l: l[0], lst)
     return check_items_equal(first_elements)
+
 
 def check_left_factors(grammar):
     for nonterminal in grammar.nonterminals:
@@ -326,11 +199,13 @@ def check_left_factors(grammar):
                     return True
     return False
 
+
 def remove_left_factoring(grammar):
     g = grammar
-    while(check_left_factors(g)):
+    while (check_left_factors(g)):
         g = __remove_left_factoring(g)
     return g
+
 
 def __remove_left_factoring(grammar):
     new_grammar = Grammar(start=grammar.start, epsilon=grammar.epsilon, eof=grammar.eof)
@@ -343,14 +218,14 @@ def __remove_left_factoring(grammar):
         if len(productions) > 1:
             prefixes = get_prefixes(grammar, productions)
             for prefix, v in prefixes.items():
-                if(len(v) == 1):
+                if (len(v) == 1):
                     new_productions.append(Rule(nonterminal, v[0]))
                     continue
-                new_x = generate_key(grammar, nonterminal)
+                new_x = __generate_key(grammar, nonterminal)
                 body = [prefix] + [new_x]
                 new_productions.append(Rule(nonterminal, body))
                 for prod in v:
-                    if(prod == []):
+                    if (prod == []):
                         new_productions.append(Rule(new_x, [grammar.epsilon]))
                     else:
                         new_productions.append(Rule(new_x, prod))
